8889c598ed6cd46b72d4189c3f399d6b8846b573
Krakinou <misterl56@hotmail.com>
2022-08-13 10:22:21 +0200

    move to openjdk-17

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -19,7 +19,7 @@ case "$mach" in
  "armv6l" ) mach="armel"
 			sha256=$sha256_armel
 			#for some reason, jre is not in the tarball
-			pkg_dependencies="$pkg_dependencies openjdk-11-jre" ;;
+			pkg_dependencies="$pkg_dependencies openjdk-17-jre" ;;
  "armv7l" ) if [ $(dpkg --print-architecture) = "armhf" ]; then 
  				mach="armhf"
  				sha256=$sha256_armhf
@@ -27,13 +27,13 @@ case "$mach" in
  				mach="armel"
  				sha256=$sha256_armel
  			fi 
- 			pkg_dependencies="$pkg_dependencies openjdk-11-jre";;
+ 			pkg_dependencies="$pkg_dependencies openjdk-17-jre";;
  "armv8l" ) mach="arm64"
  			sha256=$sha256_arm64
- 			pkg_dependencies="$pkg_dependencies openjdk-11-jre" ;;
+ 			pkg_dependencies="$pkg_dependencies openjdk-17-jre" ;;
  "aarch64" ) mach="arm64"
  			sha256=$sha256_arm64
- 			pkg_dependencies="$pkg_dependencies openjdk-11-jre" ;;
+ 			pkg_dependencies="$pkg_dependencies openjdk-17-jre" ;;
  "x86_64" ) mach="x86_64"
  			sha256=$sha256_x86_64 ;;
  * ) mach="x86" 
