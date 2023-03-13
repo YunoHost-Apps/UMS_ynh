53a584421482c4a568636d4fc5db374d10565167
Alexandre Aubin <alex.aubin@mailoo.org>
2023-03-13 22:42:48 +0100

    Re-add openjdk-17-jre dependency mistakenly removed in previous commits

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,3 +6,8 @@
 
 # dependencies used by the app
 pkg_dependencies="mediainfo dcraw p7zip"
+
+if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
+then
+    pkg_dependencies+="openjdk-17-jre"
+fi
