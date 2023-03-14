a2ea6472cef840da3db46b399f813e95be14cee8
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-14 08:02:58 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,3 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="mediainfo dcraw p7zip"
-
-if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
-then
-    pkg_dependencies+="openjdk-17-jre"
-fi
