a83c7789419738ca1ee7ee183b864969920bc5e9
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-14 07:50:27 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="mediainfo dcraw p7zip"
+#REMOVEME? pkg_dependencies="mediainfo dcraw p7zip"
 
 if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
 then
