263f93fc6e00feb8f8e7bcb2a63c19325c5228d5
Krakinou <misterl56@hotmail.com>
2023-05-03 22:21:33 +0200

    typo : add space

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,5 +9,5 @@ pkg_dependencies="mediainfo dcraw p7zip"
 
 if [[ "$YNH_ARCH" == "armhf" ]] || [[ "$YNH_ARCH" == "arm64" ]]
 then
-    pkg_dependencies+="openjdk-17-jre"
+    pkg_dependencies+=" openjdk-17-jre"
 fi
